b8e544021e251dd1172262a64390a771fe13ce23
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-03-04 23:47:18 +0900

    feat : MyFilter3를 securityConfig에 등록 (SecurityConfig가 모두 끝나고, FilterConfig에 등록된 필터들이 실행되는 것을 알 수 있음 )

--- src/main/java/com/ll/jwt/config/SecurityConfig.java
+++ src/main/java/com/ll/jwt/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ll.jwt.config;
 
+import com.ll.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -19,6 +21,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class)
                 .csrf(csrf ->
                         csrf.disable()
                 ).sessionManagement(
@@ -40,6 +43,7 @@ public class SecurityConfig {
                                         .requestMatchers("/api/v1/admin/**").hasRole("USER")
                                         .anyRequest().permitAll()
                 );
+        
         return http.build();
     }
 }
