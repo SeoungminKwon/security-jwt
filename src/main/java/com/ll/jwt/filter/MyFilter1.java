1e38a19aedb510326a978c63f9e5c4c52110fbb7
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-03-05 00:45:52 +0900

    feat: JWT를 들어가기전 간단한 테스트

--- src/main/java/com/ll/jwt/filter/MyFilter1.java
+++ src/main/java/com/ll/jwt/filter/MyFilter1.java
@@ -11,8 +11,8 @@ public class MyFilter1 implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
-        System.out.println("필터1");
-        filterChain.doFilter(servletRequest, servletResponse); // 이걸 안걸어주면 프로그램이 끝남 
 
+        System.out.println("필터1");
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 }
